f923c93ff039c373f72a4d86359cee54a8ddd919
vikukzs <vikuk.zs@gmail.com>
2017-02-26 15:40:32 +0100

    add appcompat to gradle

--- app/src/main/java/com/zsuzsannavikuk/androidtest/MainActivity.java
+++ app/src/main/java/com/zsuzsannavikuk/androidtest/MainActivity.java
@@ -1,6 +1,6 @@
 package com.zsuzsannavikuk.androidtest;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,7 +12,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
