02fabb4d07a49cdff55d3c809e775ab62fa57045
vikukzs <vikuk.zs@gmail.com>
2017-02-26 16:37:32 +0100

    add recyclerView related things

--- app/src/main/java/com/zsuzsannavikuk/androidtest/MainActivity.java
+++ app/src/main/java/com/zsuzsannavikuk/androidtest/MainActivity.java
@@ -1,23 +1,44 @@
 package com.zsuzsannavikuk.androidtest;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
-import com.greenfox.androidtest.R;
+
+import com.zsuzsannavikuk.androidtest.adapters.MoviesAdapter;
 import com.zsuzsannavikuk.androidtest.models.LoadPopularMoviesResponse;
+import com.zsuzsannavikuk.androidtest.models.Movie;
 import com.zsuzsannavikuk.androidtest.network.MovieDbManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private List<Movie> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MoviesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mAdapter = new MoviesAdapter(movieList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+
         MovieDbManager.getInstance().loadPopularMovies(1, new Callback<LoadPopularMoviesResponse>() {
             @Override
             public void onResponse(Call<LoadPopularMoviesResponse> call, Response<LoadPopularMoviesResponse> response) {
--- app/src/main/java/com/zsuzsannavikuk/androidtest/adapters/MoviesAdapter.java
+++ app/src/main/java/com/zsuzsannavikuk/androidtest/adapters/MoviesAdapter.java
@@ -1,8 +1,64 @@
 package com.zsuzsannavikuk.androidtest.adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.zsuzsannavikuk.androidtest.R;
+import com.zsuzsannavikuk.androidtest.models.Movie;
+
+import java.util.List;
+
 /**
  * Created by Zsuzska on 2017. 02. 26..
  */
 
-public class MoviesAdapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre, overview, rating;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.titleTextView);
+            genre = (TextView) view.findViewById(R.id.genresTextView);
+            year = (TextView) view.findViewById(R.id.releaseTextView);
+            overview = (TextView) view.findViewById(R.id.summaryTextView);
+            rating = (TextView) view.findViewById(R.id.ratingTextView);
+        }
+    }
+
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenres().toString());
+        holder.year.setText(movie.getReleaseDateText());
+        holder.overview.setText(movie.getOverview());
+        holder.rating.setText((int) movie.getAverageVote());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
 }
+
